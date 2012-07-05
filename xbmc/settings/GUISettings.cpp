f8d8a2625e65c1e925a19153c742d2356bcede98
FernetMenta <fernetmenta@online.de>
2012-07-05 11:44:00 +0200

    X11: fix multi-head setups

--- xbmc/settings/GUISettings.cpp
+++ xbmc/settings/GUISettings.cpp
@@ -392,11 +392,16 @@ void CGUISettings::Initialize()
   AddGroup(SETTINGS_SYSTEM, 13000);
   CSettingsCategory* vs = AddCategory(SETTINGS_SYSTEM, "videoscreen", 21373);
 
+#if defined(HAS_GLX)
+  AddString(vs, "videoscreen.monitor", 246, "", SPIN_CONTROL_TEXT);
+#endif
+
   // this setting would ideally not be saved, as its value is systematically derived from videoscreen.screenmode.
   // contains a DISPLAYMODE
 #if !defined(TARGET_DARWIN_IOS_ATV2) && !defined(TARGET_RASPBERRY_PI)
   AddInt(vs, "videoscreen.screen", 240, 0, -1, 1, 32, SPIN_CONTROL_TEXT);
 #endif
+
   // this setting would ideally not be saved, as its value is systematically derived from videoscreen.screenmode.
   // contains an index to the g_settings.m_ResInfo array. the only meaningful fields are iScreen, iWidth, iHeight.
 #if defined(TARGET_DARWIN)
