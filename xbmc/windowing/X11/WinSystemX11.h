e771f1b96d6f77358720b58362ed525cc9b9f01f
FernetMenta <fernetmenta@online.de>
2012-07-05 15:24:22 +0200

    X11: Add xbmc icon

--- xbmc/windowing/X11/WinSystemX11.h
+++ xbmc/windowing/X11/WinSystemX11.h
@@ -77,6 +77,7 @@ protected:
   GLXContext   m_glContext;
   Display*     m_dpy;
   Cursor       m_invisibleCursor;
+  Pixmap       m_icon;
   bool         m_bWasFullScreenBeforeMinimize;
   bool         m_minimized;
   bool         m_bIgnoreNextFocusMessage;
@@ -88,6 +89,7 @@ protected:
 private:
   bool IsSuitableVisual(XVisualInfo *vInfo);
   static int XErrorHandler(Display* dpy, XErrorEvent* error);
+  bool CreateIconPixmap();
 
   CStopWatch m_screensaverReset;
 };
