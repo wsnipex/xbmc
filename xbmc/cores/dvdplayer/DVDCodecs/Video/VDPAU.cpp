5ec02cfe6a7d215228640644d244851c605533af
xbmc <fernetmenta@online.de>
2013-03-06 07:35:10 +0100

    vdpau: set deinterlacing method to auto, if default method not supported

--- xbmc/cores/dvdplayer/DVDCodecs/Video/VDPAU.cpp
+++ xbmc/cores/dvdplayer/DVDCodecs/Video/VDPAU.cpp
@@ -2091,13 +2091,15 @@ void CMixer::InitCycle()
     }
     else
     {
-      CLog::Log(LOGERROR, "CMixer::%s - interlace method not supported", __FUNCTION__);
+      CLog::Log(LOGERROR, "CMixer::%s - interlace method: %d not supported, setting to AUTO", __FUNCTION__, method);
       m_mixersteps = 1;
       m_mixerfield = VDP_VIDEO_MIXER_PICTURE_STRUCTURE_FRAME;
       m_mixerInput[1].DVDPic.format = RENDER_FMT_VDPAU;
       m_mixerInput[1].DVDPic.iFlags &= ~(DVP_FLAG_TOP_FIELD_FIRST |
                                         DVP_FLAG_REPEAT_TOP_FIELD |
                                         DVP_FLAG_INTERLACED);
+
+      g_settings.m_currentVideoSettings.m_InterlaceMethod = VS_INTERLACEMETHOD_AUTO;
     }
   }
   else
