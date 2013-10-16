287ec1abc3708b688aa7275f4a7508330f8c4168
wsnipex <wsnipex@a1.net>
2013-10-16 13:37:01 +0200

    FindMySqlClient: allow override of mysql libs

--- project/cmake/modules/FindMySqlClient.cmake
+++ project/cmake/modules/FindMySqlClient.cmake
@@ -1,8 +1,10 @@
-
-find_path(MYSQLCLIENT_INCLUDE_DIRS mysql_time.h PATH_SUFFIXES mysql)
-find_library(MYSQLCLIENT_LIBRARIES NAMES mysqlclient mysqlclient_r)
+if(NOT MYSQLCLIENT_LIBRARIES AND NOT MYSQLCLIENT_INCLUDE_DIRS)
+  find_path(MYSQLCLIENT_INCLUDE_DIRS mysql_time.h PATH_SUFFIXES mysql)
+  find_library(MYSQLCLIENT_LIBRARIES NAMES mysqlclient mysqlclient_r)
+endif()
 
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(MySqlClient DEFAULT_MSG MYSQLCLIENT_LIBRARIES MYSQLCLIENT_INCLUDE_DIRS)
 mark_as_advanced(MYSQLCLIENT_LIBRARIES MYSQLCLIENT_INCLUDE_DIRS)
 list(APPEND MYSQLCLIENT_DEFINTIIONS -DHAVE_MYSQL=1)
+
