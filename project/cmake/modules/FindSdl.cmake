2f61c7433b41dce078a28df51e4a390de4dca43d
wsnipex <wsnipex@a1.net>
2013-11-02 10:05:14 +0100

    cross compile fixes for SDL

--- project/cmake/modules/FindSdl.cmake
+++ project/cmake/modules/FindSdl.cmake
@@ -5,17 +5,18 @@
 # SDL_INCLUDE_DIRS - the libSDL include directory
 # SDL_LIBRARIES - The libSDL libraries
 
-if(PKG_CONFIG_FOUND)
+if(PKG_CONFIG_FOUND AND NOT SDLIMAGE_INCLUDE_DIR)
   pkg_check_modules (SDL sdl)
-else()
+elseif(NOT SDLIMAGE_INCLUDE_DIR)
   find_package(SDL)
   list(APPEND SDL_INCLUDE_DIRS ${SDL_INCLUDE_DIR})
   list(APPEND SDL_LIBRARIES ${SDL_LIBRARIES})
+  include(FindPackageHandleStandardArgs)
+  find_package_handle_standard_args(Sdl DEFAULT_MSG SDL_INCLUDE_DIRS SDL_LIBRARIES)
+
+  list(APPEND SDL_DEFINITIONS -DHAVE_SDL=1)
+
 endif()
 
-include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(Sdl DEFAULT_MSG SDL_INCLUDE_DIRS SDL_LIBRARIES)
-
-list(APPEND SDL_DEFINITIONS -DHAVE_SDL=1)
 
 mark_as_advanced(SDL_INCLUDE_DIRS SDL_LIBRARIES SDL_DEFINITIONS)
