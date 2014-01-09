0d6779b9a49ffa6cad082308e890c64399078b52
Hendrik v. Raven <hendrik@consetetur.de>
2014-01-09 12:18:32 +0100

    Revert "declare sftp to be an internet stream"
    This reverts commit a52f47a99e0fa6a9dc9ccf6ff1ebe847ae632580.
    
    caching breaks current oportunistic loading of the sftp code

--- xbmc/utils/URIUtils.cpp
+++ xbmc/utils/URIUtils.cpp
@@ -824,8 +824,7 @@ bool URIUtils::IsInternetStream(const CURL& url, bool bStrictCheck /* = false */
       strProtocol2 == "mmsh"  || strProtocol2 == "rtsp"   ||
       strProtocol2 == "rtmp"  || strProtocol2 == "rtmpt"  ||
       strProtocol2 == "rtmpe" || strProtocol2 == "rtmpte" ||
-      strProtocol2 == "rtmps" || strProtocol2 == "sftp"   ||
-      strProtocol2 == "ssh")
+      strProtocol2 == "rtmps")
     return true;
 
   return false;
