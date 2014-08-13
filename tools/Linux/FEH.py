d7f20f3d8eefc37380a52598a5cb55c9c46d67d8
wsnipex <wsnipex@a1.net>
2014-08-13 17:41:52 +0200

    [build sys] use dynamic application name

--- tools/Linux/FEH.py
+++ tools/Linux/FEH.py
@@ -170,7 +170,7 @@ if __name__=="__main__":
         exit(0)
 
     if (badDirectRendering()):
-        error("XBMC needs hardware accelerated OpenGL rendering.\nInstall an appropriate graphics driver.\n\nPlease consult XBMC Wiki for supported hardware\nhttp://wiki.xbmc.org/?title=Supported_hardware")
+        error("Kodi needs hardware accelerated OpenGL rendering.\nInstall an appropriate graphics driver.\n\nPlease consult XBMC Wiki for supported hardware\nhttp://wiki.kodi.org/?title=Supported_hardware")
 
     if (badColorDepth()):
-        error("XBMC cannot run unless the\nscreen color depth is atleast 24 bit.\n\nPlease reconfigure your screen.")
+        error("Kodi cannot run unless the\nscreen color depth is atleast 24 bit.\n\nPlease reconfigure your screen.")
