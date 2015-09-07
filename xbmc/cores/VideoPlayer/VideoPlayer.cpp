d634457c0b5b1dbf5465ac2141dd985f7d2ca411
popcornmix <popcornmix@gmail.com>
2015-09-07 15:41:15 +0100

    fixup: Fix bug in HasFrame

--- xbmc/cores/VideoPlayer/VideoPlayer.cpp
+++ xbmc/cores/VideoPlayer/VideoPlayer.cpp
@@ -4799,7 +4799,7 @@ void CVideoPlayer::FrameWait(int ms)
 
 bool CVideoPlayer::HasFrame()
 {
-  m_renderManager.HasFrame();
+  return m_renderManager.HasFrame();
 }
 
 void CVideoPlayer::Render(bool clear, uint32_t alpha, bool gui)
