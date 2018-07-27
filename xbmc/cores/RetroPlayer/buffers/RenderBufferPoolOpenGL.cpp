911791ca81e7bc5eb563eaa8795821e3ea795b61
Wolfgang Haupt <haupt.wolfgang@gmail.com>
2018-07-27 12:09:53 +0200

    RetroPlayer: kill dependency of RenderBufferPoolOpenGL to RenderBufferPoolOpenGLES

--- xbmc/cores/RetroPlayer/buffers/RenderBufferPoolOpenGL.cpp
+++ xbmc/cores/RetroPlayer/buffers/RenderBufferPoolOpenGL.cpp
@@ -10,14 +10,23 @@
 #include "RenderBufferOpenGL.h"
 #include "cores/RetroPlayer/rendering/RenderContext.h"
 #include "cores/RetroPlayer/rendering/RenderVideoSettings.h"
+#include "cores/RetroPlayer/rendering/VideoRenderers/RPRendererOpenGL.h"
 #include "utils/GLUtils.h"
 
 using namespace KODI;
 using namespace RETRO;
 
 CRenderBufferPoolOpenGL::CRenderBufferPoolOpenGL(CRenderContext &context)
-  : CRenderBufferPoolOpenGLES(context)
+  : m_context(context)
 {
+}
+
+bool CRenderBufferPoolOpenGL::IsCompatible(const CRenderVideoSettings &renderSettings) const
+{
+  if (!CRPRendererOpenGL::SupportsScalingMethod(renderSettings.GetScalingMethod()))
+    return false;
+
+  return true;
 }
 
 IRenderBuffer *CRenderBufferPoolOpenGL::CreateRenderBuffer(void *header /* = nullptr */)
