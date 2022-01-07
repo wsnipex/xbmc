4ed55ebc1e5c61b9113674de9f1fe8dad6a77c23
graysky <graysky@archlinux.us>
2022-01-07 10:22:22 -0500

    ffmpeg: build with lto when -DUSE_LTO=ON
    I do not believe the internal ffmpeg build uses LTO if the user calls for it
    via -DUSE_LTO=ON.  This commit passes --enable-lto to ffmpeg's configure script
    if CMAKE has CMAKE_INTERPROCEDURAL_OPTIMIZATION set which -DUSE_LTO=ON does.
    
    The original author of this, loqs, points this out here[1].
    
    1. https://bugs.archlinux.org/task/69333#comment196255

--- cmake/modules/FindFFMPEG.cmake
+++ cmake/modules/FindFFMPEG.cmake
@@ -265,8 +265,9 @@ if(NOT FFMPEG_FOUND)
                                  -DCMAKE_C_COMPILER=${CMAKE_C_COMPILER}
                                  -DCMAKE_CXX_COMPILER=${CMAKE_CXX_COMPILER}
                                  -DENABLE_CCACHE=${ENABLE_CCACHE}
-                                 -DCMAKE_C_FLAGS=${CMAKE_C_FLAGS}
-                                 -DCMAKE_CXX_FLAGS=${CMAKE_CXX_FLAGS}
+                                 -DCMAKE_INTERPROCEDURAL_OPTIMIZATION=${CMAKE_INTERPROCEDURAL_OPTIMIZATION}
+                                 -DCMAKE_C_FLAGS="${CMAKE_C_FLAGS} ${CMAKE_C_COMPILE_OPTIONS_IPO}"
+                                 -DCMAKE_CXX_FLAGS="${CMAKE_CXX_FLAGS} ${CMAKE_CXX_COMPILE_OPTIONS_IPO}"
                                  -DCMAKE_EXE_LINKER_FLAGS=${LINKER_FLAGS}
                                  ${CROSS_ARGS}
                                  ${FFMPEG_OPTIONS}
